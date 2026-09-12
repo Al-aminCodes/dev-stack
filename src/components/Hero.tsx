25e79e882e3eaf1cf6470cc1e783d6160efcf005
Alamin <aloneabs16@gmail.com>
2026-09-12 14:16:20 +0600

    hero section add padding on mabai devices  doing responsive

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import hero from "../assets/banner-stack.png";
 
 const Hero = () => {
   return (
-    <header className="flex flex-col md:flex-row text-center md:text-left  justify-between items-center container mx-auto px-4 sm:px-6 lg:px-8 ">
+    <header className="flex flex-col md:flex-row text-center md:text-left  justify-between items-center container mx-auto px-4 sm:px-6 lg:px-8 pt-5 md:pt-0">
       <div className=" p-4 space-y-3">
         <h1 className="text-3xl md:text-5xl font-extrabold leading-[1.05] mb-3 md:mb-6">
           <span className="text-[#0F172A]">Build Your Ideal</span>
